6923564c4fd2530c1ee578954c4f7f55fe3d27e1
lonen8188 <112685332+lonen8188@users.noreply.github.com>
2024-03-15 11:31:15 +0900

    Update LogAdvice.java

--- 8. board(security)/src/main/java/org/zerock/aop/LogAdvice.java
+++ 8. board(security)/src/main/java/org/zerock/aop/LogAdvice.java
@@ -9,7 +9,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
-import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
 
 @Aspect
